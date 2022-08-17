dc27d11aa0fd15f010889b3b2c121d33e5af62ff
polsust <psust@easycapital.fr>
2022-08-17 11:18:27 +0200

    Remove several files from telescope search

--- .config/nvim/lua/user/plugins/telescope.lua
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -1,3 +1,5 @@
 require("telescope").setup({
-	defaults = { file_ignore_patterns = { "node_modules", ".next", ".git" } },
+	defaults = {
+		file_ignore_patterns = { "node_modules", ".next", ".git", "composer.lock", "package-lock.json", "yarn.lock" },
+	},
 })
