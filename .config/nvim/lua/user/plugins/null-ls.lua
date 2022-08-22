9dceb90f4df405aa8a9cbc7a7f86bf00b5ba9941
polsust <polsust@gmail.com>
2022-08-22 17:48:44 +0200

    Add blade_formatter to null-ls

--- .config/nvim/lua/user/plugins/null-ls.lua
+++ .config/nvim/lua/user/plugins/null-ls.lua
@@ -1,48 +1,51 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local format_on_save = true
 
-null_ls.setup {
-  sources = {
-    ---- pyhton ----
-    null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.formatting.blue,
+null_ls.setup({
+	sources = {
+		---- pyhton ----
+		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.formatting.blue,
 
-    ---- webdev ----
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.prettierd.with {
-      disabled_filetypes = { "json" },
-    },
-    null_ls.builtins.formatting.stylelint,
-    null_ls.builtins.formatting.sqlfluff.with {
-      extra_args = { "--dialect", "mysql" },
-    },
+		---- webdev ----
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.prettierd.with({
+			disabled_filetypes = { "json" },
+		}),
+		null_ls.builtins.formatting.stylelint,
+		null_ls.builtins.formatting.sqlfluff.with({
+			extra_args = { "--dialect", "mysql" },
+		}),
 
-    ---- lua ----
-    null_ls.builtins.diagnostics.selene,
-    null_ls.builtins.formatting.stylua,
+		---- lua ----
+		null_ls.builtins.diagnostics.selene,
+		null_ls.builtins.formatting.stylua,
 
-    ---- shells ----
-    null_ls.builtins.formatting.fish_indent,
-    null_ls.builtins.diagnostics.fish,
-    null_ls.builtins.formatting.shellharden,
-    null_ls.builtins.diagnostics.shellcheck,
-  },
+		---- php ----
+		null_ls.builtins.formatting.blade_formatter,
 
-  -- Format on save
-  on_attach = function(client, bufnr)
-    if format_on_save == true then
-      if client.supports_method "textDocument/formatting" then
-        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-            vim.lsp.buf.formatting_sync()
-          end,
-        })
-      end
-    end
-  end,
-}
+		---- shells ----
+		null_ls.builtins.formatting.fish_indent,
+		null_ls.builtins.diagnostics.fish,
+		null_ls.builtins.formatting.shellharden,
+		null_ls.builtins.diagnostics.shellcheck,
+	},
+
+	-- Format on save
+	on_attach = function(client, bufnr)
+		if format_on_save == true then
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+						vim.lsp.buf.formatting_sync()
+					end,
+				})
+			end
+		end
+	end,
+})
