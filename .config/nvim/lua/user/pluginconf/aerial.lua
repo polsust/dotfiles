6bb492d1e0836c0966cc4b4a4662236b8b80c567
polsust <polsust@gmail.com>
2022-09-25 18:54:48 +0200

    Neovim tweaks

--- .config/nvim/lua/user/pluginconf/aerial.lua
+++ .config/nvim/lua/user/pluginconf/aerial.lua
@@ -1,18 +1,18 @@
 local status_ok, aerial = pcall(require, "aerial")
 if not status_ok then
-  return
+	return
 end
 
 aerial.setup({
-  attach_mode = "global",
-  backends = { "lsp", "treesitter", "markdown" },
-  min_width = 28,
-  show_guides = true,
-  filter_kind = false,
-  guides = {
-    mid_item = "├ ",
-    last_item = "└ ",
-    nested_top = "│ ",
-    whitespace = "  ",
-  },
+	attach_mode = "global",
+	backends = { "lsp" },
+	min_width = 28,
+	show_guides = true,
+	filter_kind = false,
+	guides = {
+		mid_item = "├ ",
+		last_item = "└ ",
+		nested_top = "│ ",
+		whitespace = "  ",
+	},
 })
