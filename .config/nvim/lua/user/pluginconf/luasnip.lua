8646998ffcfc4fe486a8be9c03aae82f6f8258bc
polsust <psust@easycapital.fr>
2022-10-04 17:30:11 +0200

    Add react functional component with types snippet

--- .config/nvim/lua/user/pluginconf/luasnip.lua
+++ .config/nvim/lua/user/pluginconf/luasnip.lua
@@ -1,1 +1,9 @@
-require("luasnip").filetype_extend("javascript", { "javascriptreact" })
+local luasnip = require("luasnip")
+
+require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/user/user-snippets" } })
+
+luasnip.filetype_extend("javascript", { "javascriptreact" })
+
+luasnip.filetype_extend("typescriptreact", { "javascript" })
+luasnip.filetype_extend("typescript", { "javascript" })
