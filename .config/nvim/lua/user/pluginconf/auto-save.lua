478679810c791697c3e680d398eef90818b14b0d
polsust <polsust@gmail.com>
2023-02-15 17:23:43 +0100

    Add filters to neovim's autosave

--- .config/nvim/lua/user/pluginconf/auto-save.lua
+++ .config/nvim/lua/user/pluginconf/auto-save.lua
@@ -1,7 +1,32 @@
 local status_ok, autosave = pcall(require, "auto-save")
 if not status_ok then
-	return
+  return
 end
 
-autosave.setup({ noautocmd_save = true })
+autosave.setup({
+  -- function that determines whether to save the current buffer or not
+  -- return true: if buffer is ok to be saved
+  -- return false: if it's not ok to be saved
+  condition = function(buf)
+    local fn = vim.fn
+    local utils = require("auto-save.utils.data")
 
+    if fn.getbufvar(buf, "&modifiable") == 1
+        and utils.not_in(fn.getbufvar(buf, "&filetype"), { "NvimTree", "TelescopePrompt" })
+    then
+      return true -- met condition(s), can save
+    end
+    return false -- can't save
+  end,
+  write_all_buffers = false, -- write all buffers when the current one meets `condition`
+  debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+  callbacks = { -- functions to be executed at different intervals
+    enabling = nil, -- ran when enabling auto-save
+    disabling = nil, -- ran when disabling auto-save
+    before_asserting_save = nil, -- ran before checking `condition`
+    before_saving = nil, -- ran before doing the actual save
+    after_saving = nil, -- ran after doing the actual save
+  },
+
+  noautocmd_save = true,
+})
