3b7767deeb8374185c1adf3d673b1f0184862cc0
polsust <psust@easycapital.fr>
2023-02-28 09:45:07 +0100

    Add ensure installed langs for treesitter

--- .config/nvim/lua/user/pluginconf/treesitter.lua
+++ .config/nvim/lua/user/pluginconf/treesitter.lua
@@ -1,34 +1,45 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = "all",
-	-- List of parsers to ignore installing (for "all")
-	ignore_install = {},
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "php",
+    "lua",
+    "python",
+    "html",
+    "css",
+    "json",
+  },
 
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
+  auto_install = true,
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = {},
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = true,
-	},
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
 
-	indent = {
-		enable = true,
-	},
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = true,
+  },
 
-	-- PLUGINS
-	rainbow = {
-		enable = true,
-	},
+  indent = {
+    enable = true,
+  },
 
-	autotag = {
-		enable = true,
-	},
+  -- PLUGINS
+  rainbow = {
+    enable = true,
+  },
+
+  autotag = {
+    enable = true,
+  },
 })
