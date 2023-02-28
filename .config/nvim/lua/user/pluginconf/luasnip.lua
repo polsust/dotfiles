f3e560ab3d104d8b4ce931f9777e16a924d08367
polsust <psust@easycapital.fr>
2023-02-28 09:44:47 +0100

    Add js snippets to jsreact

--- .config/nvim/lua/user/pluginconf/luasnip.lua
+++ .config/nvim/lua/user/pluginconf/luasnip.lua
@@ -4,6 +4,7 @@ require("luasnip/loaders/from_vscode").lazy_load()
 require("luasnip/loaders/from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/user/user-snippets" } })
 
 luasnip.filetype_extend("javascript", { "javascriptreact" })
+luasnip.filetype_extend("javascriptreact", { "javascript" })
 
 luasnip.filetype_extend("typescriptreact", { "javascript", "javascriptreact" })
 luasnip.filetype_extend("typescript", { "javascript" })
