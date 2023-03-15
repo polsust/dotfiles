5c85c0c3374e6a5ca2ed48ce4380603acb4ebf96
polsust <polsust@gmail.com>
2023-03-15 12:59:20 +0100

    Change binds for hop.nvim

--- .config/nvim/lua/user/pluginconf/hop.lua
+++ .config/nvim/lua/user/pluginconf/hop.lua
@@ -4,9 +4,9 @@ hop.setup({ keys = "etovxqpdygfblzhckisuran" })
 local directions = require("hop.hint").HintDirection
 
 vim.keymap.set("", "f", function()
-  hop.hint_patterns()
+  hop.hint_words()
 end, { remap = true })
 
 vim.keymap.set("", "F", function()
-  hop.hint_words()
+  hop.hint_patterns()
 end, { remap = true })
