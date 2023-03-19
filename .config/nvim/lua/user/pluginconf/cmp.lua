6f10fa4a3c3d76fa3057a7fad218cde49b5045c2
polsust <polsust@gmail.com>
2023-03-19 22:58:53 +0100

    Fix vim autocomplete for commands

--- .config/nvim/lua/user/pluginconf/cmp.lua
+++ .config/nvim/lua/user/pluginconf/cmp.lua
@@ -159,12 +159,13 @@ cmp.setup.cmdline("/", {
   },
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-    { name = "nvim_lua" },
-    { name = "cmdline" },
-  }),
-})
+-- TODO: Fix this to make it work without removing regular vim autocomplete
+-- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = "path" },
+--     { name = "nvim_lua" },
+--     { name = "cmdline" },
+--   }),
+-- })
