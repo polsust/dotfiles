f2a0b343db2a1951aa7a648c133cb24c41534b0c
polsust <polsust@gmail.com>
2023-03-21 22:26:29 +0100

    Setup copilot on nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -133,6 +133,7 @@ return packer.startup(function(use)
   use({ "ziontee113/color-picker.nvim" })
   use({ "Shatur/neovim-session-manager" })
   use({ "tiagovla/scope.nvim" })
+  -- use({ "github/copilot.vim" })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
