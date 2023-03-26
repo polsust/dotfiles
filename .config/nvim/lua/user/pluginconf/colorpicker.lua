3c989665d6184dfaffc7fd9df29ea67a9b0141d9
polsust <polsust@gmail.com>
2023-03-26 12:23:52 +0200

    Change nvim colorpicker binding

--- .config/nvim/lua/user/pluginconf/colorpicker.lua
+++ .config/nvim/lua/user/pluginconf/colorpicker.lua
@@ -1,7 +1,7 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<C-c>", "<cmd>PickColor<cr>", opts)
-vim.keymap.set("i", "<C-c>", "<cmd>PickColorInsert<cr>", opts)
+vim.keymap.set("n", "<CA-c>", "<cmd>PickColor<cr>", opts)
+vim.keymap.set("i", "<CA-c>", "<cmd>PickColorInsert<cr>", opts)
 
 -- vim.keymap.set("n", "your_keymap", "<cmd>ConvertHEXandRGB<cr>", opts)
 -- vim.keymap.set("n", "your_keymap", "<cmd>ConvertHEXandHSL<cr>", opts)
