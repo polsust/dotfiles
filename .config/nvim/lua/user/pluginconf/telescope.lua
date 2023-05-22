280d19f9dbe89ffbbc8a91edef65ad31619b59b6
polsust <polsust@gmail.com>
2023-05-22 15:28:40 +0200

    Improve rg telescope ff command

--- .config/nvim/lua/user/pluginconf/telescope.lua
+++ .config/nvim/lua/user/pluginconf/telescope.lua
@@ -1,37 +1,61 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	return
+  return
 end
 local actions = require("telescope.actions")
 
+-- .*dist.*
+
+local find_command = {
+  "rg",
+  "--files",
+  "--hidden",
+  "--no-ignore", -- no gitignore
+}
+
+local ignore_patterns = {
+  -- general
+  ".git",
+  "dist",
+  "build",
+  ".build",
+
+  -- php
+  "vendor",
+
+  -- js
+  "node_modules",
+  ".next",
+  "composer.lock",
+  "package-lock.json",
+  "yarn.lock",
+}
+
+for _, ignore_pattern in ipairs(ignore_patterns) do
+  -- print(vim.inspect(#ignore_patterns + 1))
+  table.insert(find_command, "--glob")
+  table.insert(find_command, "!" .. ignore_pattern .. "")
+end
+
+print(vim.inspect(find_command))
+
 telescope.setup({
-	defaults = {
-		-- Default configuration for telescope goes here:
-		-- config_key = value,
-		mappings = {
-			i = {
-				["<C-k>"] = "move_selection_previous",
-				["<C-j>"] = "move_selection_next",
-			},
-		},
-	},
-	file_ignore_patterns = {
-		"node_modules",
-		".next",
-		".git",
-		"composer.lock",
-		"package-lock.json",
-		"yarn.lock",
-		"vendor",
-		"dist",
-		"yarn.lock",
-		"package-lock.json",
-	},
-	pickers = {
-		find_files = {
-			-- find_command = { "rg", "-uuu", "--files" },
-		},
-	},
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        ["<C-k>"] = "move_selection_previous",
+        ["<C-j>"] = "move_selection_next",
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = find_command,
+    },
+  },
 })
+
 -- telescope.load_extension("aerial")
 telescope.load_extension("persisted")
