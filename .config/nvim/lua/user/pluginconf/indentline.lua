06ed7d91d2616795e58e02cf337904fa475e50e4
polsust <psust@easycapital.fr>
2023-08-24 16:14:36 +0200

    Show indentline context

--- .config/nvim/lua/user/pluginconf/indentline.lua
+++ .config/nvim/lua/user/pluginconf/indentline.lua
@@ -9,5 +9,5 @@ vim.opt.listchars:append("eol:↴")
 indent_blankline.setup({
   show_end_of_line = true,
   show_current_context = true,
-  show_current_context_start = false,
+  show_current_context_start = true,
 })
