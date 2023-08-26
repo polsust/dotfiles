9b17e275bd9f59060c54215bad163c4403f9bfe5
polsust <polsust@gmail.com>
2023-08-26 10:44:15 +0200

    Add fn to toggle codeium

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -8,3 +8,14 @@ function ToggleAutosave()
 end
 
 vim.cmd("command! AS lua ToggleAutosave()")
+
+function ToggleCodeium()
+  vim.g.codeium_enabled = not vim.g.codeium_enabled
+  if vim.g.codeium_enabled then
+    print("Codeium enabled ✅")
+  else
+    print("Codeium disabled ❌")
+  end
+end
+
+vim.cmd("command! AI lua ToggleCodeium()")
