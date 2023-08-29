c22fe5b0628e908c019a5f5ad973b039915e0bcd
polsust <psust@easycapital.fr>
2023-08-29 14:18:30 +0200

    Add wtf plugin nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -126,6 +126,32 @@ local plugins = {
     "phelipetls/jsonpath.nvim",
     ft = { "json" },
   },
+  {
+    "piersolenski/wtf.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "gw",
+        mode = { "n" },
+        function()
+          require("wtf").ai()
+        end,
+        desc = "Debug diagnostic with AI",
+      },
+      {
+        mode = { "n" },
+        "gW",
+        function()
+          require("wtf").search()
+        end,
+        desc = "Search diagnostic with Google",
+      },
+    },
+  },
 }
 
 require("lazy").setup(plugins)
