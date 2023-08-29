3659a84b0dee97a6a1ef3567a457eab082155371
polsust <polsust@gmail.com>
2023-08-29 17:47:29 +0200

    Add keybind to open peek markdown previewer

--- .config/nvim/lua/user/pluginconf/peek.lua
+++ .config/nvim/lua/user/pluginconf/peek.lua
@@ -19,3 +19,5 @@ require("peek").setup({
 
 vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
 vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+
+vim.keymap.set("n", "<leader>po", "<cmd>PeekOpen<CR>", opts)
