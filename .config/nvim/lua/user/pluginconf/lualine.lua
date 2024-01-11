a21912589454d12ee5271178170810f71aeb1a24
polsust <psust@easycapital.fr>
2024-01-11 11:12:41 +0100

    Show macro recording on statusline

--- .config/nvim/lua/user/pluginconf/lualine.lua
+++ .config/nvim/lua/user/pluginconf/lualine.lua
@@ -7,6 +7,15 @@ local wtf = require("wtf")
 
 require("lualine").setup({
   sections = {
-    lualine_x = { wtf.get_status },
+    lualine_x = {
+      wtf.get_status,
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = {
+          fg = "#ff9e64",
+        },
+      },
+    },
   },
 })
