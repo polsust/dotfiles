26adf0e1d1bab1753538db9a86d4cc0878fe464c
polsust <psust@kidsandus.com>
2025-05-07 19:26:00 +0200

    Make autocmd

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -118,7 +118,15 @@ vim.api.nvim_create_autocmd({ "User" }, {
   end,
 })
 
+vim.opt.autoread = true
 
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
 -- Notification after file change
 vim.api.nvim_create_autocmd("FileChangedShellPost", {
