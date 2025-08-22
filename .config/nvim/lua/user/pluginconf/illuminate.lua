7ebfff498b2183230a4170c86e9354e849f38a67
polsust <polsust@gmail.com>
2025-08-22 16:37:02 +0200

    Change illuminate conf

--- .config/nvim/lua/user/pluginconf/illuminate.lua
+++ .config/nvim/lua/user/pluginconf/illuminate.lua
@@ -1,16 +1,16 @@
 require("illuminate").configure({
-	-- providers: provider used to get references in the buffer, ordered by priority
-	providers = {
-		"lsp",
-		"treesitter",
-		"regex",
-	},
-	-- delay: delay in milliseconds
-	delay = 100,
-	filetypes_denylist = {
-		"alpha",
-		"NvimTree",
-		"TelescopePrompt",
-	},
+  -- providers: provider used to get references in the buffer, ordered by priority
+  providers = {
+    "regex",
+    "lsp",
+    "treesitter",
+  },
+  -- delay: delay in milliseconds
+  delay = 0,
+  filetypes_denylist = {
+    "alpha",
+    "NvimTree",
+    "TelescopePrompt",
+  },
 })
 -- vim.cmd([[hi def IlluminatedWordText gui=underline]])
