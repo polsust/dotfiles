a9838caf1689bfc900343db0d462136664420818
polsust <polsust@gmail.com>
2025-10-01 16:16:20 +0200

    WIP: Make autosave not save in the last 10 seconds

--- .config/mpv/scripts/autosave.lua
+++ .config/mpv/scripts/autosave.lua
@@ -20,14 +20,23 @@ local o = {
 options.read_options(o)
 
 local mp = require("mp")
+-- mp.enable_messages("debug")
 
 local function save()
+  local current_time = mp.get_property_number("time-pos")
+  local total_time = mp.get_property("duration")
+  local remaining_time = total_time - current_time
+
+  if remaining_time < 10 then
+    return
+  end
+
   mp.command("write-watch-later-config")
 end
 
 local save_period_timer = mp.add_periodic_timer(o.save_period, save)
 
-local function pause(name, paused)
+local function pause(_name, paused)
   if paused then
     save_period_timer:stop()
   else
