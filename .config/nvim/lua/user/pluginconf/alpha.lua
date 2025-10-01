61e9a150f42183f535f526424d88e1237dc398e1
polsust <polsust@gmail.com>
2025-10-01 16:23:35 +0200

    Tweak alpha conf

--- .config/nvim/lua/user/pluginconf/alpha.lua
+++ .config/nvim/lua/user/pluginconf/alpha.lua
@@ -1,24 +1,26 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[███╗  ██╗██╗   ██╗██╗███╗   ███╗]],
-	[[████╗ ██║██║   ██║██║████╗ ████║]],
-	[[██╔██╗██║╚██╗ ██╔╝██║██╔████╔██║]],
-	[[██║╚████║ ╚████╔╝ ██║██║╚██╔╝██║]],
-	[[██║ ╚███║  ╚██╔╝  ██║██║ ╚═╝ ██║]],
-	[[╚═╝  ╚══╝   ╚═╝   ╚═╝╚═╝     ╚═╝]],
+  [[  ██████   █████                   █████   █████  ███                  ]],
+  [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
+  [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
+  [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
+  [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
+  [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
+  [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
+  [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
+  [[                                                                       ]],
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("p", " " .. " Find project", ":Telescope persisted<CR>"),
-	dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/lua/user <CR>"),
-	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+  dashboard.button("p", " " .. " Find project", ":Telescope persisted<CR>"),
+  dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/lua/user <CR>"),
+  dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
-
