5599cdea61ac03c0345d05d4a62ce05855bfb449
polsust <polsust@gmail.com>
2025-10-07 01:00:00 +0200

    Delete garbage comments

--- .config/nvim/lua/plugins/mason/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason/mason-lspconfig.lua
@@ -9,23 +9,4 @@ return {
   opts = {
     automatic_enable = true,
   },
-  -- config = function(_,opts)
-  --   require("mason-lspconfig").setup(opts)
-  --
-  --   -- it works like this but not the other way on the ./lsp/servername.lua
-  --   vim.lsp.config['jsonls'] = {
-  --     settings = {
-  --       json = {
-  --         schemas = require("schemastore").json.schemas(),
-  --         validate = { enable = true },
-  --       },
-  --     },
-  --   }
-  --
-  --
-  --
-  -- end,
 }
-
--- outside of mason
--- setup_lsp("gdscript")
