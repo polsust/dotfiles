4946ad764ea4982e354d18ff50954e92c13bb5b6
polsust <polsust@gmail.com>
2025-10-06 21:41:24 +0200

    Install doc plugin for godot

---
+++ .config/nvim/lua/plugins/lsp/gdscript-extended-lsp.lua
@@ -0,0 +1,15 @@
+return {
+  enabled = false,
+  "teatek/gdscript-extended-lsp.nvim",
+  opts = {
+    -- doc_file_extension = ".txt", -- Documentation file extension (can allow a better search in buffers list with telescope)
+    -- view_type = "vsplit", -- Options : "current", "split", "vsplit", "tab", "floating"
+    -- split_side = false, -- (For split and vsplit only) Open on the right or top on false and on the left or bottom on true
+    -- keymaps = {
+    --   declaration = "gd", -- Keymap to go to definition
+    --   close = { "q", "<Esc>" }, -- Keymap for closing the documentation
+    -- },
+    -- floating_win_size = 0.8, -- Floating window size
+    -- picker = "snacks", -- Options : "telescope", "snacks"
+  },
+}
