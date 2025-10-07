1a5b4de1edc0d2956e53b1a1d8c2dea21952fdd7
polsust <polsust@gmail.com>
2025-10-07 14:17:23 +0200

    Add keybind

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -6,6 +6,7 @@ return {
     { "<leader>gj", "<cmd>Gitsigns next_hunk<CR>" },
     { "<leader>gk", "<cmd>Gitsigns prev_hunk<CR>" },
     { "<leader>gh", "<cmd>Gitsigns reset_hunk<CR>" },
+    { "<leader>gH", "<cmd>Gitsigns reset_buffer<CR>" },
     { "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>" },
     { "<leader>gd", "<cmd>Gitsigns diffthis<CR>" },
     { "<leader>gl", "<cmd>Gitsigns blame_line<CR>" },
